8486d792db6de10985cac12c87b8c815017f889b
915827225@qq.com <gl515xxx>
2015-09-19 07:03:58 +0800

    Signed-off-by: 915827225@qq.com <gl515xxx>

---
+++ src/main/java/com/lovecws/lucene/LUCENEUtil/App.java
@@ -0,0 +1,13 @@
+package com.lovecws.lucene.LUCENEUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
